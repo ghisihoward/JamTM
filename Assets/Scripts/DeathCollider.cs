d089be76ddfe8e9fd9736dcf00414e97d24c9173
Vanessa <vfchristoff9@gmail.com>
2018-05-20 00:14:56 -0300

    DeathCollider script added, objects rearranged

---
+++ Assets/Scripts/DeathCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathCollider : MonoBehaviour {
+
+	//this sees when other colliders make contact with this collider
+	void OnCollisionEnter2D(Collision2D coll) {
+	Destroy (coll.gameObject);
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,21 +6,20 @@ public class GameManager : MonoBehaviour {
 
 	public GameObject objectsPrefab;
 	float timeLeft = 5.0f;
-	int x = 11
-
+	float x = 10.30f;
+	
 	void Start () {
 		objectsPrefab = GameObject.Find ("FloatingObjects");
 	//	Blitzkrieg.GetGameObjectPosition ();
 	}
 
 	void ObjectSpawn () {
-		GameObject objectsSpawner = Instantiate (objectsPrefab, new Vector3 (x,-3,0), Quaternion.Euler(0,0,0));
+		GameObject objectsSpawner = Instantiate (objectsPrefab, new Vector3 (x,-2.9f,0), Quaternion.Euler(0,0,0));
 			objectsSpawner.GetComponent <Rigidbody2D> ().AddForce (new Vector2 (-80, 0));
 			Debug.Log("Hello i exist");
 		//if (x < -12) {
 		//	GameObject.Destroy (objectsPrefab);
 		}
-	}
 
 	void Update () {
 		timeLeft -= Time.deltaTime;
@@ -29,12 +28,11 @@ public class GameManager : MonoBehaviour {
 			ObjectSpawn ();
 		}
 	}
-
+}
 // > Bagulinho aparece na tela de jogo :: Soma 1 nas coisas que apareceram 					> Apareceram = 1
 // > Jogador tem que reagir e reage    :: Soma 1 nas vezes que ele reagiu positivamente 	> Acertou 	 = 1
 // > Bagulinho aparece na tela de jogo :: Soma 1 nas coisas que apareceram 					> Apareceram = 2
 // > Jogador tem que reagir e não 	   :: Não soma 1 nas vezes que ele reagiu positivamente > Acertou    = 1
 
 // Acertou / Apareceram > 0~100% de 2 estrelas. 0 - 0 est, 25, 1/2 estrela... etc.
-}
 	
